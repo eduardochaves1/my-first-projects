0e4a1285c9c45e72cf19b4075f430af2a1af9bb9
Eduardo Chaves <henriqueduardo2002@gmail.com>
2022-08-31 01:18:19 -0300

    update on `array-searching.js` file

--- array-searching.js
+++ array-searching.js
@@ -38,12 +38,50 @@ const booksByCategory = [
 // quantidade de categorias
 console.log(`Nº de Categorias: ${booksByCategory.length}`)
 
-// quantidade de autores FALTA TERMINAR..
-for (let nAuthor of booksByCategory) {
-   console.log(`Nº de Autores: ${nAuthor.books.length}`)
-}
-
 // quantidade de livros por categoria
 for (let nBooks of booksByCategory) {
    console.log(`Nº de Livros da Categoria "${nBooks.category}": ${nBooks.books.length}`)
 }
+
+// quantidade de autores
+function getAuthorsCount() {
+   let authors = []
+   for (let category of booksByCategory) {
+      for (let book of category.books) {
+         if (authors.indexOf(book.author) == -1) {
+            authors.push(book.author)
+         }
+      }
+   }
+   return authors.length
+}
+console.log(`Nº de Autores: ${getAuthorsCount()}`)
+
+// livros do autor Augusto Cury
+/* function getCuryBooks() {
+   let curyBooks = []
+   for (let category of booksByCategory) {
+      for (let book of category.books) {
+         if (book.author == "Augusto Cury") {
+            curyBooks.push(book.title)
+         }
+      }
+   }
+   return curyBooks
+}
+console.log(`Livros do autor Augusto Cury: ${getCuryBooks()}`) */
+
+// livros de um autor específico
+let authorName = "T. Harv Eker"
+function getAuthorBooks() {
+   let authorBooks = []
+   for (let category of booksByCategory) {
+      for (let book of category.books) {
+         if (book.author == authorName) {
+            authorBooks.push(book.title)
+         }
+      }
+   }
+   return authorBooks
+}
+console.log(`Livros do autor ${authorName}: ${getAuthorBooks()}`)
